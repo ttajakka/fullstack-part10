cd9ce596aa9459a8a6fe92db7ab64e9d56e5920c
ttajakka <tuomas.tajakka@gmail.com>
2023-07-14 12:28:54 +0300

    add 10.5. update 10.4: make tab pressable

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, Pressable, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
 import Text from './Text';
@@ -9,23 +9,31 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.backgroundDark,
+    flexDirection: "row"
   },
   tab: {
     paddingVertical: 20,
-    paddingHorizontal: 15
+    paddingHorizontal: 15,
   }
 });
 
 const AppBarTab = ({ label }) => {
-  return <View>
-    <Text style={styles.tab} color={"white"} fontSize={'subheading'} fontWeight={"bold"}>{label}</Text>
-  </View>
+  return <Pressable onPress={() => null}>
+    <Text
+      style={styles.tab}
+      color={"white"}
+      fontSize={'subheading'}
+      fontWeight={"bold"}
+    >
+      {label}
+    </Text>
+  </Pressable>
 }
 
 const AppBar = () => {
-  return <View style={styles.container}>{
-    <AppBarTab label="Repositories"/>
-  }</View>;
+  return <View style={styles.container}>
+    <AppBarTab label="Repositories" />
+  </View>;
 };
 
 export default AppBar;
--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,4 +1,58 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
+import Text from "./Text";
+
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  topRow: {
+    flexDirection: 'row',
+    padding: 15
+  },
+  infoCol: {
+    // alignItems: "flex-start",
+    paddingRight: 20,
+    marginHorizontal: 20
+  },
+  tinyLogo: {
+    width: 45,
+    height: 45,
+    borderRadius: 4
+  },
+  languagePill: {
+    backgroundColor: theme.colors.primary,
+    color: theme.colors.white,
+    borderRadius: 3,
+    paddingVertical: 4,
+    paddingHorizontal: 6
+  },
+  numbersRow: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    paddingBottom: 15
+  },
+  numberItem: {
+    alignItems: "center"
+  },
+  bottomDivider: {
+    height: 10,
+    backgroundColor: "#EEEEEE"
+  }
+});
+
+const NumberItem = ({ num, label }) => {
+
+  const toKnotation = (n) => {
+    if (n < 1000) return n.toString();
+
+    return (n / 1000).toFixed(1).toString() + "k";
+  }
+  return (
+    <View style={styles.numberItem}>
+      <Text fontWeight="bold" style={{ paddingBottom: 5 }}>{toKnotation(num)}</Text>
+      <Text color="textSecondary">{label}</Text>
+    </View>
+  )
+}
 
 const RepositoryItem = ({
   fullName,
@@ -7,17 +61,36 @@ const RepositoryItem = ({
   forksCount,
   stargazersCount,
   ratingAverage,
-  reviewCount
+  reviewCount,
+  ownerAvatarUrl
 }) => {
   return (
     <View>
-      <Text>Full name: {fullName}</Text>
-      <Text>Description: {description}</Text>
-      <Text>Language: {language}</Text>
-      <Text>Stars: {stargazersCount}</Text>
-      <Text>Forks: {forksCount}</Text>
-      <Text>Revies: {reviewCount}</Text>
-      <Text>Rating: {ratingAverage}</Text>
+      <View style={styles.topRow}>
+        <Image style={styles.tinyLogo} source={{ uri: ownerAvatarUrl }} />
+        <View style={styles.infoCol}>
+          <Text style={{ paddingBottom: 8 }}
+            fontSize="subheading"
+            fontWeight="bold"
+          >
+            {fullName}
+          </Text>
+            <Text style={{ paddingBottom: 8 }}
+              color="textSecondary"
+            >
+              {description}
+            </Text>
+          <View style={{ flexDirection: "row" }}><Text style={styles.languagePill}>{language}</Text></View>
+        </View>
+
+      </View>
+      <View style={styles.numbersRow}>
+        <NumberItem num={stargazersCount} label={"Stars"} />
+        <NumberItem num={forksCount} label={"Forks"} />
+        <NumberItem num={reviewCount} label={"Reviews"} />
+        <NumberItem num={ratingAverage} label={"Rating"} />
+      </View>
+      <View style={styles.bottomDivider}></View>
     </View>
   )
 }
